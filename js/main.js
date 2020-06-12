487a7065f75e10393a276d8f9bed13c6190e27f5
Семён Шатилов <shatilov00@inbox.ru>
2020-06-12 18:20:18 +0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function () {
+    // const header = document.querySelector('.header');
+    // window.onscroll = () => {
+    //     if (window.pageYOffset > 50){
+    //         console.log(window.pageYOffset)
+    //         header.classList.add('.header_active');
+    //     }
+    // };
+    window.onload = function(){
+        header = document.getElementById('head');
+        header.style.backgroudColor = '#2F58CA';
+        console.log('OK')
+    }
+}());  
